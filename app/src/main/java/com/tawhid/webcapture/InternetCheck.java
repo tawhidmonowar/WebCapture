f650afa167e10962d7d34204dd732b7fbe78d90c
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-08-25 01:37:45 +0600

    Create InternetCheck.java

---
+++ app/src/main/java/com/tawhid/webcapture/InternetCheck.java
@@ -0,0 +1,22 @@
+package com.tawhid.webcapture;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class InternetCheck {
+
+    public static boolean isConnected(PDFActivity context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    public static void doSomethingWhenConnected(PDFActivity context) {
+        // Perform your actions when there's internet connection
+    }
+
+    public static void doSomethingWhenDisconnected(PDFActivity context) {
+        // Perform your actions when there's no internet connection
+    }
+}
